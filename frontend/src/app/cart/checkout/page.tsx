bdaa6c6c22a5d88939518f73263612bfbde34fd5
wayne-yang <wayne.yang7021@gmail.com>
2024-05-22 16:52:36 +0800

    checkout page

--- frontend/src/app/cart/checkout/page.tsx
+++ frontend/src/app/cart/checkout/page.tsx
@@ -1,22 +1,69 @@
 import Image from "next/image"
+import { useRouter } from "next/navigation"
 export default function CheckoutPage(){
+    const router = useRouter()
+    const handleBacktoCart = () =>{
+        router.push("./cart")
+    }
     return(
         <div>
-            <div className="m-10 flex flex-col items-center px-5">
-                <div className="flex flex-col items-center gap-2 mb-3">
-                    <p className="text-4xl">Checkout</p>
-                    <p>your order</p>
+            <div className="m-10 flex flex-col items-center px-5 gap-5">
+                <div className="flex flex-row justify-between w-full items-center">
+                    <button className="bg-buttons font-semibold hover:bg-gray-100 border border-black rounded-md py-3 text-sm text-black uppercase w-1/5 h-3/5">Back</button>
+                    <div className="flex flex-col items-center gap-2 mb-1 w-1/3">
+                        <p className="text-4xl">Checkout</p>
+                        <p>your order</p>
+                    </div>
+                    <button className="bg-buttons font-semibold hover:bg-gray-100 border border-black rounded-md py-3 text-sm text-black uppercase w-1/5 h-3/5">Confirm Checkout</button>
+                    
+                    {/* <div className="flex justify-between">
+                        <button className="bg-buttons font-semibold hover:bg-gray-100 border border-black rounded-md py-3 text-sm text-black uppercase w-full">Back</button>
+                        <button className="bg-buttons font-semibold hover:bg-gray-100 border border-black rounded-md py-3 text-sm text-black uppercase w-full">Confirm Checkout</button>
+                    </div>
+                    <div className="flex flex-col items-center gap-2 mb-1 w-1/3">
+                        <p className="text-4xl">Checkout</p>
+                        <p>your order</p>
+                    </div> */}
                 </div>
                 <div className="w-full bg-gray-300 flex flex-row px-10 py-4 gap-8">
-                    <Image
-                    // src={`${productDetails[0].imageLink}`}
-                    src = {"/story.jpg"}  
-                    alt="book_pic"
-                    width={200}
-                    height={50}
-                    className="max-h-28 min-h-28 max-w-36 min-w-36 border border-black rounded-md"
-                    ></Image>
-                    <p>Wi</p>
+                    <div className="flex flex-row gap-8 w-1/2">
+                        <Image
+                        // src={`${productDetails[0].imageLink}`}
+                        src = {"/story.jpg"}  
+                        alt="book_pic"
+                        width={200}
+                        height={50}
+                        className="max-h-28 min-h-28 max-w-36 min-w-36 border border-black rounded-md"
+                        ></Image>
+                        <div className="flex flex-col">
+                            <p>Title: </p>
+                            <p>Author:</p>
+                        </div>
+                    </div>
+                    <div className="flex flex-row w-1/2 justify-between py-5 px-20 items-center">
+                        <p>X1</p>
+                        <p>380 NTD</p>
+                    </div>
+                </div>
+                <div className="w-full bg-gray-300 flex flex-row px-10 py-4 gap-8">
+                    <div className="flex flex-row gap-8 w-1/2">
+                        <Image
+                        // src={`${productDetails[0].imageLink}`}
+                        src = {"/story.jpg"}  
+                        alt="book_pic"
+                        width={200}
+                        height={50}
+                        className="max-h-28 min-h-28 max-w-36 min-w-36 border border-black rounded-md"
+                        ></Image>
+                        <div className="flex flex-col">
+                            <p>Title: </p>
+                            <p>Author:</p>
+                        </div>
+                    </div>
+                    <div className="flex flex-row w-1/2 justify-between py-5 px-20 items-center">
+                        <p>X1</p>
+                        <p>380 NTD</p>
+                    </div>
                 </div>
             </div>
             <div className="m-10 flex flex-col items-center px-5 gap-5">
