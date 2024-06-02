237059f4d186a16ecc0a77727f9a2bd8aca739ae
Jasper Chan <b09705026@ntu.edu.tw>
2024-06-02 20:35:57 +0800

    index.ts changed and types.ts added

---
+++ backend/types.ts
@@ -0,0 +1,30 @@
+export type booksType = {
+    id: Number,
+    title: String,
+    author: String,
+    price: Number,
+    sales: Number,
+    genre: [String],
+    publisher: String,
+    publishDate: Date,
+    language: String,
+    image: String,
+    description: String,
+  };
+  
+  export type cartItem ={
+    itemId: Number,
+    quantity: Number,
+  }
+  
+  export type userType = {
+    _id: String,
+    username: String,
+    password: String,
+    email: String,
+    phone: String,
+    address: String,
+    favorite: [Number],
+    cart: [cartItem],
+    salesCart: [Number]
+  };
