b830e14a423cefa3afbd2dc9bb9f83c5089ab8cf
Hank1222 <119921064+HankWu37878@users.noreply.github.com>
2024-06-05 13:15:59 +0800

    Update page.tsx

--- frontend/src/app/login/congradulation/page.tsx
+++ frontend/src/app/login/congradulation/page.tsx
@@ -12,9 +12,9 @@ export default function Home() {
     return (
     <main className="flex min-h-screen border-2">
         <div className="flex flex-col w-1/2 rounded-lg mx-auto justify-center items-center">
-            <h1 className="text-6xl">Congradulation!</h1>
-            <div className='flex w-full justify-start border border-black rounded-lg mt-16 text-nowrap py-16 px-10 text-center text-xl'>
-                We have send an mail to your e-mail account to help you reset the password. Please reset the password via the link in the mail within 2 days.
+            <h1 className="text-6xl">Congratulation!</h1>
+            <div className='flex w-full justify-start border border-black rounded-lg mt-16 break-all py-16 px-10 text-center text-xl'>
+                We have sent an mail to your e-mail account to help you reset the password. Please reset the password via the link in the mail within 2 days.
             </div>
             
             <button className="mt-10 px-4 py-2 text-2xl rounded-md border border-black hover:bg-slate-200 hover:shadow-2xl bg-buttons" onClick={handleHome}>Back to home</button>
